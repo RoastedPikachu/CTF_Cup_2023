de4535ecf6fd6e4ccec2ec24de002b307630c29f
Boris Karabut <boriskarabytwork@gmail.com>
2023-09-03 10:11:25 +0300

    Доверстал страницу 'Жюри'

--- src/app/jury/page.tsx
+++ src/app/jury/page.tsx
@@ -26,15 +26,127 @@ const Page = () => {
         <>
             {!isMobile ? <TheHeader/> : <TheMobileHeader/>}
 
-            <main className='px-[10%] w-full'>
+            <main className='px-[10%] w-full pb-[100px]'>
                 <img src='/static/juryPage/text/JuryText.svg' alt='Жюри' className='w-[55%]'/>
 
                 <PersonInfo
                     imgPath={'/static/juryPage/avatars/BudnikovAlexandrAvatar.svg'}
                     personDescription={'Будников Александр'}
-                    personText={'Председатель оргкомитета — действительный член АИН, член-корреспондент РАЕН и Академии Криптографии, лауреат Государственной премии, к.ф-м.н'}
-                    mt={80}
-                    ml={200}
+                    personText={'Управляющий директор по информационной безопасности, ПАО АФК «Система»'}
+                    mt={'80px'}
+                    ml={'200px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/BugrovVladimirAvatar.svg'}
+                    personDescription={'Бугров Владимир'}
+                    personText={'И. О. начальника Департамента по обеспечению информационной безопасности ПФР'}
+                    mt={'-10px'}
+                    ml={'600px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/VolkovSergeyAvatar.svg'}
+                    personDescription={'Волков Сергей'}
+                    personText={'Начальник отдела информационной безопасности, АО «ГОЗНАК»'}
+                    mt={'80px'}
+                    ml={'200px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/DerlishIlyaAvatar.svg'}
+                    personDescription={'Дерлыш Илья'}
+                    personText={'Эксперт информационной безопасности, Росатом'}
+                    mt={'-80px'}
+                    ml={'800px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/KaderMichailAvatar.svg'}
+                    personDescription={'Кадер Михаил'}
+                    personText={'Архитектор по информационной безопасности'}
+                    mt={'100px'}
+                    ml={'0px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/KuznetsovAndreyAvatar.svg'}
+                    personDescription={'Кузнецов Андрей'}
+                    personText={'Технический директор Национального Киберполигона «Ростелеком-Солар»'}
+                    mt={'-80px'}
+                    ml={'600px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/MarkovAlexeyAvatar.svg'}
+                    personDescription={'Марков Алексей'}
+                    personText={'Президент группы компаний ГК «Эшелон»'}
+                    mt={'50px'}
+                    ml={'200px'}
+                />
+
+                <PersonInfo
+                    imgPath={''}
+                    personDescription={'Минаков Сергей'}
+                    personText={'Старший научный сотрудник Академия криптографии РФ'}
+                    mt={'-80px'}
+                    ml={'800px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/MasalovichAndreyAvatar.svg'}
+                    personDescription={'Масалович Андрей'}
+                    personText={'Генеральный директор ООО «Лавина Пульс»'}
+                    mt={'40px'}
+                    ml={'400px'}
+                />
+
+                <PersonInfo
+                    imgPath={''}
+                    personDescription={'Михайленко Наталья'}
+                    personText={'Доцент кафедры противодействия преступлениям в сфере ИТТ, к.ю.н.'}
+                    mt={'50px'}
+                    ml={'800px'}
+                />
+
+                <PersonInfo
+                    imgPath={''}
+                    personDescription={'Лоскутова Олеся'}
+                    personText={'Заместитель начальника центрального вычислительного центра, Главный информационно-аналитический центр МВД России'}
+                    mt={'-180px'}
+                    ml={'0px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/UsanovAlexeyAvatar.svg'}
+                    personDescription={'Усанов Алексей'}
+                    personText={'Технический директор НТЦ «Вулкан»'}
+                    mt={'40px'}
+                    ml={'400px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/OvchinnikovViktorAvatar.svg'}
+                    personDescription={'Овчинников Виктор'}
+                    personText={'Начальник отдела информационной безопасности, ООО «М13 ИТ УСЛУГИ»'}
+                    mt={'140px'}
+                    ml={'0px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/SklyarovDmitriyAvatar.svg'}
+                    personDescription={'Скляров Дмитрий'}
+                    personText={'Руководитель отдела анализа приложений, Positive Technologies'}
+                    mt={'-160px'}
+                    ml={'600px'}
+                />
+
+                <PersonInfo
+                    imgPath={'/static/juryPage/avatars/TsarevGrigoriyAvatar.svg'}
+                    personDescription={'Царев Григорий'}
+                    personText={'Руководитель направления Центра взаимодействия и реагирования Департамента информационной безопасности Банка России'}
+                    mt={'120px'}
+                    ml={'200px'}
                 />
             </main>
 
--- src/widgets/shared/PersonInfo.tsx
+++ src/widgets/shared/PersonInfo.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const PersonInfo = ({imgPath, personDescription, personText, mt, ml}) => {
     return (
-        <div className={`flex justify-between mt-[${mt}px] ml-[${ml}px] w-[390px] h-[220px]`}>
-            <img src={imgPath} alt={personDescription} className='w-[180px] h-full'/>
+        <div className={`flex justify-between mt-[${mt}] ml-[${ml}] w-[390px] h-[220px]`}>
+            {imgPath ? <img src={imgPath} alt={personDescription} className='w-[180px] h-full'/> : <div className='w-[180px] h-full bg-[#b8b8b8]'></div>}
 
             <span className='w-[190px]'>
                 <p className='text-[#bf5af2] text-[1.25rem] font-["Good_Timing"] font-bold'>{personDescription.toUpperCase()}</p>
