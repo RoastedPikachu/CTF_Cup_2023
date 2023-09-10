8bf8c751c6bf93522159c052ef074e8635c7ac1c
Boris Karabut <boriskarabytwork@gmail.com>
2023-09-10 09:56:30 +0300

    Исправление билда

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,21 +22,6 @@ export default function RootLayout({
   return (
       <html lang="ru">
         <body className={inter.className}>
-            <script type="text/javascript" >
-                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-                ym(94888158, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true
-                });
-            </script>
-            <noscript><div><img src="https://mc.yandex.ru/watch/94888158" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-
             <ReduxProvider>
                 {children}
             </ReduxProvider>
