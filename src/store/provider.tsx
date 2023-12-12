31bb07b73b90296bae6b25d8e1f4ea729ae50c9b
Boris Karabut <boriskarabytwork@gmail.com>
2023-12-12 15:07:29 +0300

    Заменил изображения программы мероприятий

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -1,16 +1,15 @@
-'use client';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
+"use client";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 
-import { store, persistor } from '@/store/index';
+import { store, persistor } from "@/store/index";
 
-export function ReduxProvider({children}: {children: React.ReactNode}) {
-    return (
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                { children }
-            </PersistGate>
-        </Provider>
-    )
+export function ReduxProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
-
